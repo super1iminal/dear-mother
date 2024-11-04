4fc3a7e45a6f5ca2ccfdc3e1d4dc930070a44022
UBC Student <mskelton@students.cs.ubc.ca>
2024-11-03 21:06:52 -0800

    Check merge compatibility (#66)
    * fixed item UI and health UI bugs
    
    * added door assets and pause menu button assets. fixed item UI lag
    
    * fixed animations bugging out when enemies were killed
    
    * Fix music on death, add mesh bullets, and re-add FPS counter
    
    * enemy 2 animations into Main (#65)
    
    * Stop enemies from spawning too close
    
    * undid last thing bcz it froze sometimes, item pictures in
    
    * Added second enemy type animation
    
    * enemy types; only yellow can shoot now
    
    * floor items with one bug-spawning on other stuff
    
    * projectile enemies are slower
    
    * enemy_2 walk
    
    * enemy 2 attack animation
    
    * Enemy 2 animations (#64)
    
    * fixed item UI and health UI bugs
    
    * added door assets and pause menu button assets. fixed item UI lag
    
    * fixed animations bugging out when enemies were killed
    
    * Fix music on death, add mesh bullets, and re-add FPS counter
    
    ---------
    
    Co-authored-by: kelcyw <kelcy.w.wang@gmail.com>
    Co-authored-by: UBC Student <kelcyw@students.cs.ubc.ca>
    Co-authored-by: UBC Student <bhandal2@students.cs.ubc.ca>
    
    ---------
    
    Co-authored-by: mskelton <a4t4d@ugrad.cs.ubc.ca>
    Co-authored-by: kelcyw <kelcy.w.wang@gmail.com>
    Co-authored-by: UBC Student <kelcyw@students.cs.ubc.ca>
    Co-authored-by: UBC Student <bhandal2@students.cs.ubc.ca>
    
    ---------
    
    Co-authored-by: kelcyw <kelcy.w.wang@gmail.com>
    Co-authored-by: UBC Student <kelcyw@students.cs.ubc.ca>
    Co-authored-by: UBC Student <bhandal2@students.cs.ubc.ca>
    Co-authored-by: mskelton <a4t4d@ugrad.cs.ubc.ca>

--- src/world_init.cpp
+++ src/world_init.cpp
@@ -178,7 +178,9 @@ Entity createEnemy(RenderSystem* renderer, vec2 position, float speed, ivec2 roo
 	auto& deadly = registry.deadlys.emplace(entity);
 	deadly.t = std::chrono::high_resolution_clock::now();
 	deadly.t_patrol = std::chrono::high_resolution_clock::now();
-	deadly.type = (int) rand() % 2;  // 0 for grey melee, 1 for slower yellow projectile
+
+  deadly.type = (int) rand() % 2;  // 0 for grey melee, 1 for slower yellow projectile
+
 	if (registry.deadlys.get(entity).type == 1) {
 		registry.motions.get(entity).max_speed = 0.7 * speed;
 		auto& shooter = registry.shooters.emplace(entity);
