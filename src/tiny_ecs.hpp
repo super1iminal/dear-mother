54fa84c39713d342653386d162c598d9007abcbe
UBC Student <hcui12@students.cs.ubc.ca>
2024-11-15 18:47:10 -0800

    Revert "merge fix"

--- src/tiny_ecs.hpp
+++ src/tiny_ecs.hpp
@@ -119,7 +119,7 @@ public:
         }
         if (map_entity_componentID.count(e) == 0)
         {
-            static_assert(has_compareFunction<Component>::value , "Component does not have compareFunction defined");
+            static_assert(has_compareFunction<Component>::value && "Component does not have compareFunction defined");
             bool inserted = false;
             for (int i = 0; i < entities.size(); i++) {
                 if (c.compareFunction(c, get(entities[i])) < 1) {  // if this entity should come before entities[i]
